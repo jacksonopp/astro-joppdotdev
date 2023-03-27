35db1d70e71a4c46ff6124df7bb8c3f5c2fd964e
Jackson Oppenheim <jackson.e.oppenheim@healthpartners.com>
2023-03-27 09:20:11 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    svelte(),
-    tailwind(),
-    mdx(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [svelte(), tailwind(), mdx(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
+  adapter: vercel()
 });
