f43e5e4504ba5d5dcb7389eac3c6068d35b4fe93
Jackson Oppenheim <jackson.e.oppenheim@healthpartners.com>
2023-04-03 14:46:08 -0500

    add partytown and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,17 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    svelte(),
-    tailwind(),
-    mdx(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [svelte(), tailwind(), mdx(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), partytown()],
   adapter: vercel({
-    analytics: true,
-  }),
+    analytics: true
+  })
 });
